cf823d8e4e144fe4cb77b5d7d245c600839c327d
Poljak Marijan <marijan.poljak@croatiaairlines.hr>
2022-03-07 12:22:10 +0100

    Zadnja verzija sa dodavanjem modela i migracija

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,12 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+    
+});
+
+Route::get('/hello', function () {
+    //return view('welcome');
+    return "Hello world";
 });
 
 Auth::routes();
